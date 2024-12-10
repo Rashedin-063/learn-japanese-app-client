a3ee21c9de99cbb66bef56ab7f0fe64dcaf4b71e
Rashedin-063 <rashedinislam.06@gmail.com>
2024-12-10 17:10:24 +0600

    theme provider added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ThemeProvider from './providers/ThemeProvider.jsx'
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider>
+      <HelmetProvider>
     <App />
+      </HelmetProvider> 
+     </ThemeProvider>
   </StrictMode>,
 )
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'faded-pearl': '#E6DFAF',
+        'midnight-gray': '	##3c3c3c',
+        'deep-ocean': '#004080',
+        'green-lantern': '#046645',
+        'pure-white': '#FFFFFF',
+        'charcoal-gray': '#000000',
+      },
+    },
+  },
+  plugins: [],
+};
+
