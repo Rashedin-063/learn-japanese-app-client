1b084f9c04984591fea49f1d607f1589c2167699
Rashedin-063 <rashedinislam.06@gmail.com>
2024-12-11 12:50:59 +0600

    footer updated

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -4,34 +4,36 @@ const Footer = () => {
   return (
     <div className='bg-slate-950'>
       <div>
-        <footer className='footer text-white items-center px-[26px] lg:px-6 py-[26.5px] max-w-7xl mx-auto flex justify-between font-wendy'>
-          <aside className=' items-center'>
+        <footer
+          className={`footer text-zen-serenity items-center px-[26px] lg:px-6 py-[26.5px] max-w-7xl mx-auto flex justify-between `}
+        >
+          <aside>
             <div className='flex gap-4 items-center'>
               <svg
-                width='36'
-                height='36'
+                width='40'
+                height='40'
                 viewBox='0 0 24 24'
                 xmlns='http://www.w3.org/2000/svg'
                 fillRule='evenodd'
                 clipRule='evenodd'
-                className='fill-green-heaven'
+                className='fill-green-heaven mt-2'
               >
                 <path d='M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z'></path>
               </svg>
               <div>
                 <Link
                   to='/lessons'
-                  className='text-lg lg:text-xl font-bold cursor-pointer flex items-center font-playwrite text-zen-serenity'
+                  className='text-lg lg:text-xl font-bold cursor-pointer flex items-center font-playwrite'
                 >
-                  Learn Japanese
+                  <span className='text-amber-glow'> Learn Japanese</span>
                 </Link>
-                <p className='text-xs font-extra-light opacity-75 mt-1 font-yujiMai'>
+                <p className='text-xs font-extra-light mt-1 font-yujiMai'>
                   Copyright © ${new Date().getFullYear()} - All right reserved
                 </p>
               </div>
             </div>
           </aside>
-          <footer className='grid-flow-col gap-4 place-self-center justify-self-end lg:pr-8'>
+          <div className='grid-flow-col gap-4 place-self-center justify-self-end lg:pr-8'>
             <a>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
@@ -65,7 +67,7 @@ const Footer = () => {
                 <path d='M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z'></path>
               </svg>
             </a>
-          </footer>
+          </div>
         </footer>
       </div>
     </div>
