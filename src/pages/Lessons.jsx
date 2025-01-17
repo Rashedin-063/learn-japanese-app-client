b300ef47d67b72f064c7aa225a7988defc471cbb
Rashedin-063 <rashedinislam.06@gmail.com>
2025-01-17 16:14:08 +0600

    lesson card updated

--- src/pages/Lessons.jsx
+++ src/pages/Lessons.jsx
@@ -2,8 +2,8 @@ import { Helmet } from "react-helmet-async";
 import useLoadLessons from './../hooks/useLoadLessons';
 import LoadingSpinner from "../components/LoadingSpinner";
 import ErrorMessage from "../components/ErrorMessage";
-import PageTitle from "../components/PageTitle";
 import { Link } from 'react-router-dom';
+import { FaHourglassStart } from 'react-icons/fa';
 import { GiOpenBook } from 'react-icons/gi';
 import bg from '../assets/sc2.jpg';
 
@@ -25,8 +25,8 @@ const Lessons = () => {
         {lessons?.map((lesson) => (
           <div
             key={lesson._id}
-            className='group rounded-xl border-2 border-amber-glow drop-shadow-2xl transition-translate duration-500 ease-out hover:-translate-y-2 
-                     hover:border-green-700 animate-fade-in border-b-[6px] border-r-[6px]'
+            className='group rounded-xl border-2 border-green-700 drop-shadow-2xl transition-translate duration-500 ease-out hover:-translate-y-2 
+                     hover:border-amber-glow animate-fade-in border-b-[6px] border-r-[6px] relative'
             style={{
               backgroundImage: `url(${bg})`,
               backgroundPosition: 'top',
@@ -36,7 +36,7 @@ const Lessons = () => {
           >
             <div className='flex flex-col space-y-1 p-6 relative overflow-hidden'>
               <div
-                className='w-12 h-12 bg-amber-glow/50 rounded-full flex items-center justify-center 
+                className='w-12 h-12 bg-amber-glow/30 rounded-full flex items-center justify-center 
                           group-hover:scale-110 transition-all duration-500 ease-out
                           group-hover:bg-green-heaven/20 group-hover:translate-x-2'
               >
@@ -56,16 +56,16 @@ const Lessons = () => {
               </p>
             </div>
 
-            <div className='flex items-center p-6 pt-0'>
+            <div className='flex items-center p-6 pt-0 absolute bottom-2 right-2'>
               <Link
                 to={`/single-vocabulary/${lesson.lessonNumber}`}
                 className='w-full'
               >
                 <button
-                  className='w-full inline-flex items-center justify-center gap-2 
-                           bg-green-heaven text-white py-3 px-4 rounded-md
+                  className='w-48 inline-flex items-center justify-center gap-2 
+                           bg-green-900 text-white py-2 px-4 rounded-md
                            transition-[transform,background-color,box-shadow] duration-500 ease-out
-                           hover:bg-green-700 hover:shadow-lg
+                           hover:bg-green-700 shadow-lg
                            active:scale-95
                             relative overflow-hidden
                            before:absolute before:inset-0 
@@ -75,7 +75,7 @@ const Lessons = () => {
                           '
                 >
                   Start Learning
-                  <GiOpenBook className='w-4 h-4' />
+                  <FaHourglassStart className='w-4 h-4' />
                 </button>
               </Link>
             </div>
