40a60a8e8eea61521641a4882d5875cea923d035
Rashedin-063 <rashedinislam.06@gmail.com>
2025-01-17 18:32:16 +0600

    lesson card updated

--- src/pages/Lessons.jsx
+++ src/pages/Lessons.jsx
@@ -16,17 +16,17 @@ const Lessons = () => {
   if (isError) return <ErrorMessage error={error} />;
 
   return (
-    <div className='container mx-auto px-4 py-8'>
+    <div className='container mx-auto px-4 py-8 '>
       <Helmet>
         <title>Learn Japanese || Lessons</title>
       </Helmet>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-6 '>
         {lessons?.map((lesson) => (
           <div
             key={lesson._id}
-            className='group rounded-xl border-2 border-green-700 drop-shadow-2xl transition-translate duration-500 ease-out hover:-translate-y-2 
-                     hover:border-amber-glow animate-fade-in border-b-[6px] border-r-[6px] relative'
+            className='group rounded-xl border-2 border-green-heaven/80 drop-shadow-2xl transition-translate duration-500 ease-out hover:-translate-y-2 
+                     hover:border-amber-glow/80 animate-fade-in border-b-[6px] border-r-[6px] relative h-60 '
             style={{
               backgroundImage: `url(${bg})`,
               backgroundPosition: 'top',
@@ -36,9 +36,9 @@ const Lessons = () => {
           >
             <div className='flex flex-col space-y-1 p-6 relative overflow-hidden'>
               <div
-                className='w-12 h-12 bg-amber-glow/30 rounded-full flex items-center justify-center 
+                className='w-12 h-12 bg-green-heaven/20 rounded-full flex items-center justify-center 
                           group-hover:scale-110 transition-all duration-500 ease-out
-                          group-hover:bg-green-heaven/20 group-hover:translate-x-2'
+                          group-hover:bg-amber-glow/40 group-hover:translate-x-2'
               >
                 <GiOpenBook className='w-6 h-6 text-crimson-gate rotate-12' />
               </div>
@@ -56,16 +56,13 @@ const Lessons = () => {
               </p>
             </div>
 
-            <div className='flex items-center p-6 pt-0 absolute bottom-2 right-2'>
-              <Link
-                to={`/single-vocabulary/${lesson.lessonNumber}`}
-                className='w-full'
-              >
+            <div className='flex  p-6 pt-0 items-end absolute right-0 bottom-'>
+              <Link to={`/single-vocabulary/${lesson.lessonNumber}`}>
                 <button
-                  className='w-48 inline-flex items-center justify-center gap-2 
-                           bg-green-900 text-white py-2 px-4 rounded-md
+                  className=' inline-flex items-center justify-center gap-2 
+                           bg-green-heaven/80 text-white py-2 px-4 rounded-md
                            transition-[transform,background-color,box-shadow] duration-500 ease-out
-                           hover:bg-green-700 shadow-lg
+                           group-hover:bg-amber-glow/80 shadow-lg
                            active:scale-95
                             relative overflow-hidden
                            before:absolute before:inset-0 
@@ -74,8 +71,8 @@ const Lessons = () => {
                            hover:before:translate-x-[150%]
                           '
                 >
-                  Start Learning
-                  <FaHourglassStart className='w-4 h-4' />
+                  Start Lesson
+                  {/* <FaHourglassStart className='w-4 h-4' /> */}
                 </button>
               </Link>
             </div>
